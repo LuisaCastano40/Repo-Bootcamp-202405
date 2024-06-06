666908e43b8cb754cb03a3e684e77c9a1c02c489
Luisa Castaño <luisa.castano40@gmail.com>
2024-06-05 22:01:14 -0500

    [actualización] objetos y funciones

--- actividadesClase/javascript/conceptosBasicos/scripts/conceptosBasicos.js
+++ actividadesClase/javascript/conceptosBasicos/scripts/conceptosBasicos.js
@@ -42,10 +42,104 @@ const nombre = 'luisa';
 
 // booleanos -> verdadero o falso
 
-let esVerdadero = true;
-const esFalso = false;
+let esVerdadero = true; //true -> verdadero
+const esFalso = false; //false -> falso
 
 // arreglos -> guardar màs de un dato -> DEL MISMO TIPO
 // se reconocen por el uso de [] y los datos se separan por comas
 let arregloNumeros = [5, 6, 7, 3, 9];
 const arregloNombres = ['luisa', 'manuela', 'daniel'];
+
+//POO -> Programación orientada a objetos
+//Objetos -> son datos que representan en CÓDIGO, elementos o cosas de la vida real para que podamos darles instrucciones lógicas
+
+//Todos los elementos de la vida real los podemos definir, en términos de CARACTERÍSTICAS (atributos (código)), de ACCIONES (métodos (código))
+
+/*
+    [] -> corchetes con los arreglos de datos
+    {} -> llaves los tipos de datos objeto
+    () -> métodos -> funciones
+
+    estructura -> clave valor -> los atributos y métodos se separan por comas
+
+    Yo puedo acceder no sólo al objeto completo, sino a sus propiedades y a sus métodos, de forma específica,
+
+    Como:
+    nombreObjeto.atributo
+    nombreObjeto.método
+*/
+
+const persona1 = {
+    //clave: valor;
+    //Atributos
+    nombre: 'Luisa',
+    edad:23,
+    altura: '170 cm',
+    nacionalidad: 'Colombiana',
+    //métodos -> funciones
+    saludar: function(){
+       console.log('Hola desarrolladores')
+    },
+    despedir: function(){
+        console.log('Chao, desarrolladores')
+    }
+
+}
+
+let productos = {
+    nombre: 'computador',
+    precio: 10000,
+    cantidad: 20,
+    descripcion: 'es muy rápido',
+    comprar: function(){
+        console.log('comprando producto')
+    }
+}
+
+
+// ==================== FUNCIONES =================
+
+/*  
+¿CÓMO SE USAN LAS FUNCIONES EN JS?
+
+    1. Declaración típica de funciones
+    2. Funciones anónimas -> a funciones sin nombre
+    3. Funciones flecha -> los estándares modernos de programación en js
+
+    4. Funciones incorporadas -> funciones que ya vienen establecidas dentro de js, para que nostros los podamos usar
+
+*/
+
+//1. DECLARACIÓN BÁSICA DE FUNCIONES
+/* 
+    function nombreFuncion(){
+        lógica dentro de la funcion
+    }
+
+*/
+
+function saludar(){
+    console.log('holiiiiiiiiiiiiiiiiiiiiii');
+}
+
+//2. FUNCIONES ANÓNIMAS
+let despedida = function (){
+    console.log('chauuuuuuuuu')
+}
+
+//3. FUNCIONES FLECHA -> Forma moderna 
+
+let suma = () => {
+    console.log(5+4);
+}
+
+// 4. FUNCIONES INCORPORADAS
+    /*
+    
+        alert -> enviarle un mensaje al usuario
+        prompt -> es para pedir datos al usuario
+        parseInt -> convertir un dato a entero
+        parseFloat -> convertir un dato a flotante(decimal)
+        console -> nos envía mensaje a la consola del desarrolador
+        Math -> nos permite hacer operacione smatemáticas más complejas
+    */
--- actividadesClase/javascript/conceptosBasicos/scripts/index.js
+++ actividadesClase/javascript/conceptosBasicos/scripts/index.js
@@ -2,3 +2,14 @@
 /* comentario en css - comentario en bloque de js*/
 // comentario de lìnea en js
 
+/*
+Ejercicio 1: Suma de dos números
+    Escribe una función que solicite dos números al usuario, calcule la suma de esos números y muestre el resultado en un alert.
+*/
+
+
+/*
+    Ejercicio 2: Número par o impar
+        Enunciado:
+        Escribe una función que solicite un número al usuario y muestre en un alert si el número es "par" o "impar".
+*/
