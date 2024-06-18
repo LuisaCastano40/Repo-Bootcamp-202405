aa0f34b911f44722df3806f8257243c6d0ad5f16
Luisa Castaño <luisa.castano40@gmail.com>
2024-06-17 21:48:22 -0500

    [actualizacion] ejercicio 3

---
+++ actividadesClase/javascript/actividad6/scripts/Ejercicio3.js
@@ -0,0 +1,29 @@
+console.log('ESTE ES EL ÚLTIMO EJERCICIO');
+
+// CICLOS
+
+/*
+    -> For -> necesitamos saber cuántas veces se va a repetir el bloque de código
+    -> ForEach -> nos recorre arreglos de datos [1, 2, 3, 4, 5], repite el bloque de código según la cantidad de elementos dentro de mi arreglo de datos
+    -> While -> no necesariamente debe saber cuantas veces va a repetir el bloque de código
+    -> Do While -> Siempre nos va a ejecutar el bloque de código al menos una vez
+*/
+
+
+/*
+3.) Mostrar números:
+Escribe un programa que use un bucle for para imprimir los números del 1 al 10 en la consola.
+*/
+
+/* contadores -> ayudan a determinar las veces que debe repetir el bloque de código 
+    aux
+    contador
+    i, j
+
+    1. cuando inicia
+    2. cuando termina
+    3. incremento -> i++ / decremento i--
+*/
+for(let i=1; i<=10; i++){
+    console.log(i);
+}
